38e6072e6989bff43adb860cbda699f3ae063126
GanYihuan <583520052@qq.com>
2018-11-15 06:04:31 +0800

    format

---
+++ server.js
@@ -0,0 +1,11 @@
+﻿const http = require('http')
+
+http
+	.createServer(function(request, response) {
+		console.log('request come', request.url)
+		response.end('123')
+	})
+	.listen(8888)
+
+console.log('server listen 8888')
+ 
