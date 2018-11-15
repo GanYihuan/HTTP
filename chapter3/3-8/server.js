ebf404809e1fedd9a2c970de47f0521165b33cad
GanYihuan <583520052@qq.com>
2018-11-15 09:26:58 +0800

    3-9

---
+++ chapter3/3-8/server.js
@@ -0,0 +1,24 @@
+﻿const http = require('http')
+const fs = require('fs')
+
+http
+  .createServer(function (request, response) {
+    console.log('request come', request.url)
+    const html = fs.readFileSync('test.html', 'utf-8')
+    const img = fs.readFileSync('../3-3/3-3-01.png')
+    if (request.url === '/') {
+      const html = fs.readFileSync('test.html', 'utf-8')
+      response.writeHead(200, {
+        'Content-Type': 'text/html'
+      })
+      response.end(html)
+    } else {
+      response.writeHead(200, {
+        'Content-Type': 'img/png'
+      })
+      response.end(img)
+    }
+  })
+  .listen(8888)
+
+console.log('server listen 8888')
---
+++ chapter3/3-9/server.js
@@ -0,0 +1,26 @@
+﻿const http = require('http')
+const fs = require('fs')
+
+http
+  .createServer(function (request, response) {
+    console.log('request come', request.url)
+
+    if (request.url === '/') {
+      // 301: 永久跳转, 会从缓存里面读
+      // 302: 临时跳转
+      response.writeHead(302, {
+        'Location': '/new'
+      })
+      response.end('')
+    }
+
+    if (request.url === '/new') {
+      response.writeHead(302, {
+        'Content-Type': 'text/html'
+      })
+      response.end('<div>this is content</div>')
+    }
+  })
+  .listen(8888)
+
+console.log('server listen 8888')
